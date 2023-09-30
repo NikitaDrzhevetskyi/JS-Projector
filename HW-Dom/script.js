b3c3a5ccbe01bd834fed5ed415e8a7ede4c17a0d
NikitaDrzhevetskyi <n.drzhevetskyi@gmail.com>
2023-09-30 20:52:03 +0300

    HW with Dom is done

---
+++ HW-Dom/script.js
@@ -0,0 +1,13 @@
+'use strict'
+//1
+let titleColor = document.querySelector('h2#headerTwo');
+titleColor.style.backgroundColor = 'blue'
+//2
+let borderInSection = document.querySelector('section.firstSection');
+borderInSection.style.border = "2px solid black";
+//3
+let listItem = document.querySelector('ul li:nth-child(5)');
+listItem.style.color = "red";
+//4 
+let elementWithClass = document.querySelector('.hatredLevelBlock');
+elementWithClass.style.fontSize = "48px";
