4fac3055997c84017eaec718674758d759edf684
NikitaDrzhevetskyi <n.drzhevetskyi@gmail.com>
2023-10-04 17:27:05 +0300

    HW with Dom is fixed

--- HW-Dom/script.js
+++ HW-Dom/script.js
@@ -1,13 +1,17 @@
-'use strict'
+"use strict";
 //1
-let titleColor = document.querySelector('h2#headerTwo');
-titleColor.style.backgroundColor = 'blue'
+let titleColor = document.getElementById("headerTwo");
+titleColor.style.backgroundColor = "blue";
 //2
-let borderInSection = document.querySelector('section.firstSection');
+let borderInSection = document.querySelector("section > h2");
 borderInSection.style.border = "2px solid black";
 //3
-let listItem = document.querySelector('ul li:nth-child(5)');
-listItem.style.color = "red";
-//4 
-let elementWithClass = document.querySelector('.hatredLevelBlock');
-elementWithClass.style.fontSize = "48px";
+let listItem = document.querySelectorAll("li");
+listItem.forEach((item) => {
+  if (item.innerText === "Пункт 5") {
+    item.style.color = "red";
+  }
+});
+//4
+let elementWithClass = document.getElementsByClassName("hatredLevelBlock");
+elementWithClass[0].style.fontSize = "36px";
